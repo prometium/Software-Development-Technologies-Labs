fea13ae8458039b47014424e3bb26fec6a0e5953
Prometium <akrasnovskii@yandex.ru>
2019-11-01 13:40:11 +0300

    add gitpages environment

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
     main: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, './docs')
   },
   resolve: {
     alias: {
